78db60e7a81a2c6d8c11cd212e21436ffba471e2
KJY <KJY@SALTLUX-KIM-JONG-YOUNG>
2022-11-28 14:20:09 +0900

    JPA Entity, Repository 스캔 패키지 설정 및 권한 엔티티 Primary Key 지정

--- board-cores/board-jpa-core/src/main/java/edu/jong/board/jpa/config/DatabaseConfig.java
+++ board-cores/board-jpa-core/src/main/java/edu/jong/board/jpa/config/DatabaseConfig.java
@@ -3,10 +3,12 @@ package edu.jong.board.jpa.config;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -14,9 +16,13 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
+import edu.jong.board.BoardConstants;
+
 @Configuration
 @EnableJpaAuditing
 @EnableTransactionManagement
+@EntityScan(basePackages = BoardConstants.ROOT_PACKAGE)
+@EnableJpaRepositories(basePackages = BoardConstants.ROOT_PACKAGE)
 public class DatabaseConfig {
 
 	@Bean  
--- board-services/board-role-service/src/main/java/edu/jong/board/role/entity/RoleEntity.java
+++ board-services/board-role-service/src/main/java/edu/jong/board/role/entity/RoleEntity.java
@@ -4,6 +4,9 @@ import javax.persistence.Column;
 import javax.persistence.Convert;
 import javax.persistence.Converter;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -27,6 +30,8 @@ import lombok.ToString;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class RoleEntity extends BaseTimeEntity {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long no;
 
 	@NotBlank
